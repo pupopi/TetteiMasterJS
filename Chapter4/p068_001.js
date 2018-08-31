b05702e69fc18aaac6299562665d44387f46cbf7
pupopi <pupopipepan@gmail.com>
2018-08-31 15:57:56 +0900

    配列リテラルによる生成

---
+++ Chapter4/p068_001.js
@@ -0,0 +1,11 @@
+// 配列リテラルによる生成
+
+var evens = [2, 4, 6, 8];
+
+var empty[];
+console.log(empty);
+
+var a = [2, 4];
+console.log(a);
+
+var various = [ 3.14 , "pi", true, {x:1, y:2}, [2, 4, 6, 8] ];
