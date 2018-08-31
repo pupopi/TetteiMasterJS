96f9975a8cdfb0014d69f25cb751c5db962c3a23
pupopi <pupopipepan@gmail.com>
2018-08-31 15:28:18 +0900

    コンストラクタの役割

---
+++ Chapter4/p061_001.js
@@ -0,0 +1,10 @@
+// コンストラクタの役割
+
+function Card(suit, rank){
+    this.suit = suit ;
+    this.rank = rank ;
+}
+
+var card1 = new Card("ハート", "A");
+var card2 = new Card("クラブ", "K");
+var card3 = new Card("スペード", "2");
