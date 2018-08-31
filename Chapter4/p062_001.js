7cf0f133741f59819aa9fe026396d01c305418d6
pupopi <pupopipepan@gmail.com>
2018-08-31 15:32:39 +0900

    座標と速度を持つオブジェクト

---
+++ Chapter4/p062_001.js
@@ -0,0 +1,11 @@
+// 座標と速度を持つオブジェクト
+
+function Particle(x, y, vx, vy){
+    this.x = x;
+    this.y = y;
+    this.vx = vx;
+    this.vy = vy;
+    this.velocity = Math.sqrt(vx*vx+vy*vy)
+}
+var p = new.Particle(0, 0, 3, 4);
+console.log(p);
