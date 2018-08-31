4fcd7f1e03c57f7aa23e2fe8743b41860be8e2e6
pupopi <pupopipepan@gmail.com>
2018-08-31 15:52:41 +0900

    Dateコンストラクタ

---
+++ Chapter4/p065_001.js
@@ -0,0 +1,14 @@
+// Dateコンストラクタ
+
+var now = new Date;
+console.log(now);
+
+veer then = new Date(2008, 5, 10);
+console.log(then);
+
+var elapsed = now - then;
+console.log(elapsed);
+
+var start = new Date();
+var end = new Date();
+var elapsed = end - start;
