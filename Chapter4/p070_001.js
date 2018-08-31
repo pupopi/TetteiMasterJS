5244c6c17f1ea12badfeaa153b221a480616c304
pupopi <pupopipepan@gmail.com>
2018-08-31 16:03:40 +0900

    Arrayコンストラクタ

---
+++ Chapter4/p070_001.js
@@ -0,0 +1,8 @@
+// Arrayコンストラクタ
+
+var evens = new Array(2,4,6,8);
+var empty = new Array();
+var various = new Array(3.14, "pi", true, {x:1,y:2}, [2,4,6,8]);
+
+var x = new Array(3);
+console.log(x.length);
