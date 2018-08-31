62dd2f576418841c3cc0ec500ecfd683b6d9e9a4
pupopi <pupopipepan@gmail.com>
2018-08-31 12:03:57 +0900

    テンプレートリテラルの基本

---
+++ p041_002.js
@@ -0,0 +1,11 @@
+// テンプレートリテラルの基本
+
+var t = `Man errs as long as
+he strives.`;
+
+var t = "Man errs as long as\nhe strives."
+
+var t = String.raw`Man errs as long as\nhe strives.`;
+
+var t = "Men errs as long as\\nhe strives.";
+
