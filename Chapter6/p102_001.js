b2af51a29d7ee632a47968422d9767c962a454be
pupopi <pupopipepan@gmail.com>
2018-09-06 10:59:11 +0900

    コンソールへのテキスト出力01

---
+++ Chapter6/p102_001.js
@@ -0,0 +1,9 @@
+// ダイアログ表示
+
+alert("こんにちは！");
+
+var name = prompt("名前を入力してください", "鈴木大翔");
+var age = parseInt(prompt("年齢を入力してください"));
+var height = parseFloat(prompt("身長を入力してください"));
+
+var ret = confirm("リンクを表示してよろしいですか？");
---
+++ Chapter6/p104_001.js
@@ -0,0 +1,6 @@
+// コンソールへのテキスト出力01
+
+var a = [2, 4, 6];
+console.log("配列",a,"の長さは",a.length,"です。");
+
+console.log("配列"+a+"の長さは"+a.length+"です。");
