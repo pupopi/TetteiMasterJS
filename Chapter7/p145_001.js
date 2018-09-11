49cbccda752451256647d087b29667267c4a0a3f
pupopi <pupopipepan@gmail.com>
2018-09-11 10:26:33 +0900

    二項演算を行う関数

---
+++ Chapter7/p145_001.js
@@ -0,0 +1,19 @@
+// 二項演算を行う関数
+
+function binaryOperation(a, b, operator){
+    switch(operator){
+        case "+" : return a+b;
+        case "-" : return a-b;
+        case "*" : return a*b;
+        case "/" : return a/b;
+        case "%" : return a%b;
+        case "^" : return Math.pow(a,b);
+        default : return NaN;
+    }
+}
+
+console.log(binaryOperation(2,3,"+"));
+console.log(binaryOperation(2,3,"-"));
+console.log(binaryOperation(2,3,"*"));
+console.log(binaryOperation(2,3,"^"));
+console.log(binaryOperation(2,3,"A"));
