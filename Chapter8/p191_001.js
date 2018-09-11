b799db1dfef476b460bd3a9b76486112ecfa627e
pupopi <pupopipepan@gmail.com>
2018-09-11 17:55:41 +0900

    カウンタ関数を作る関数

--- Chapter8/p191_001.js
+++ Chapter8/p191_001.js
@@ -1,7 +1,8 @@
 // カウンタ関数を作る関数
 
 function makeCounter(){ // 無名関数
-    var count = 0;
+    var coun
+    t = 0;
     return f;
     function f(){   // 無名関数がクロージャ
         return count++;　// スコープチェーンにより外側からcountを参照
