5995df3d14a4327dfd59cea0eac3d6c5a70795af
pupopi <pupopipepan@gmail.com>
2018-09-11 10:20:29 +0900

    Switch文

---
+++ Chapter7/p144_001.js
@@ -0,0 +1,15 @@
+// Switch文の例
+
+switch(n) {
+    case 1:
+        console.log("One");
+        break;
+    case 2:
+        console.log("Two");
+        break;
+    case 3:
+        console.log("Three");
+        break;
+    default:
+        console.log("Other");
+}
