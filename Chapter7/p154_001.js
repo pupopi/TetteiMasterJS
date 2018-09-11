cb86ba0b4bb18cd0725258acfb618d7fd480b92d
pupopi <pupopipepan@gmail.com>
2018-09-11 11:09:37 +0900

    配列の要素の合計を求める

---
+++ Chapter7/p154_001.js
@@ -0,0 +1,11 @@
+// 配列の要素の合計を求める
+
+function sumArray(a){
+    var sum = 0;
+    for( var i=0; i<a.length; i++) {
+        sum += a[i];
+    }
+    return sum;
+}
+var a = [3,5,1,2,6,7]
+console.log(sumArray(a));
