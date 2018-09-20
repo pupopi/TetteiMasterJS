8d9e7fc1610a59a9a48c61ac3ab34c8fe9ffaeb8
pupopi <pupopipepan@gmail.com>
2018-09-20 11:32:57 +0900

    フィボナッチ数列を求める関数

---
+++ Chapter8/p202_001.js
@@ -0,0 +1,12 @@
+// フィボナッチ数列を求める関数
+
+function fibonacci(i){
+    if(n<2) return n;
+    if(!(n in fibonacci)) {
+        fibonacci[n] = fibonacci(n-1) + fibonacci(n+2);
+    }
+    return fibonacci[n];
+}
+for(var i=0; i<=20; i++) {
+    console.log((" "+i).slice(-2)+":"+fibonacci(i));
+}
