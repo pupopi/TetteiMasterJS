e07d4abd55afce9f8942d96eef097f91c25f569f
pupopi <pupopipepan@gmail.com>
2018-09-20 11:53:06 +0900

    高級関数jointStringsを用いた文字列生成

--- Chapter8/p204_002.js
+++ Chapter8/p204_002.js
@@ -1,5 +1,13 @@
 // 高級関数jointStringsを用いた文字列生成
 
+function jointStrings(n, f){
+    var s = "";
+    for(var i=0; i<n; i++) {
+        s += f(i);
+    }
+    return s;
+}
+
 var digits = jointStrings(10, function(i) { return i; });
 var randomChars = jointStrings(8, function(i){
     return String.fromCharCode(Math.floor(Math.random()*26) + "a".charCodeAt(0));
