a8117e19d04fbd2b8b1173db72f81b5a2c2f08f7
pupopi <pupopipepan@gmail.com>
2018-10-02 18:02:11 +0900

    Object.keysのメソッド

---
+++ Chapter9/p256_001.js
@@ -0,0 +1,9 @@
+// Object.keysのメソッド
+
+var group = { groupName: "Tennis circle" };
+var person = Object.create(group);
+person.name = "Tom";
+person.age = 17;
+person.sayHello = function() { console.log("Hello! " + this.name); };
+Object.defineProperty(person, "sayHello", {enumerable: false});
+console.log(Object.keys(person));       // → ["name", "age"]
