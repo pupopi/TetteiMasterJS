b1b742a91fe830a47836fa658d34cc0eac13e076
pupopi <pupopipepan@gmail.com>
2018-10-02 18:13:25 +0900

    Mixin関数

---
+++ Chapter9/p259_001.js
@@ -0,0 +1,10 @@
+// Mixin関数
+
+function mixin(target, source) {
+    for(var property in source) {
+        if(source.hasOwnProperty(property)) {
+            target[property] = source[property];
+        }
+    }
+    return target;
+}
