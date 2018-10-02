ed2320c8dd1e484eaa29e9b2bc7b4011919d5dfe
pupopi <pupopipepan@gmail.com>
2018-10-02 18:16:14 +0900

    より完全なMixin関数

---
+++ Chapter9/p261_001.js
@@ -0,0 +1,10 @@
+// より完全なMixin関数
+
+function mixin(target, source) {
+    var keys = Object.keys(source);
+    for(var i=0; i<keys.length; i++) {
+        var descriptor = Object.getOwnPropertyDescriptor(source, keys[i]);
+        Object.defineProperty(target, keys[i], descriptor);
+    }
+    return target;
+}
