fa6187d2bc2173f2befde885d884f4aab36c48e3
pupopi <pupopipepan@gmail.com>
2018-10-02 18:10:59 +0900

    Object.k保護：Object.sealメソッド

---
+++ Chapter9/p258_002.js
@@ -0,0 +1,13 @@
+// 保護：Object.sealメソッド
+
+var person = { name: "Tom" };
+Object.seal(person);
+person.age = 17;
+delete person.name;
+Object.defineProperty(person, "name", {enumerable: false});
+console.log("name" in person);      // → true: nameは削除されていない
+console.log("age" in person);       // → false: ageは追加されていない
+console.log(Object.getOwnPropertyDescriptor(person, "name"));
+// → {value: "Tom", writable: true, enumerable: true, configurable: false}
+person.name = "Huck";
+console.log(person);        // → Object {name: "Huck"}
