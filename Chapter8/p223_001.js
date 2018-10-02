b481b6232791f1ec3957d9b3d22367e11460d9ce
pupopi <pupopipepan@gmail.com>
2018-10-02 12:28:41 +0900

    HTMLで使用できない文字をエスケープ

---
+++ Chapter8/p223_001.js
@@ -0,0 +1,21 @@
+// HTMLで使用できない文字をエスケープ
+
+function htmlEscape(strings, ...values) {
+    var result = strings[0];
+    for(var i=0; i<values.length; i++) {
+        result += escape(values[i]) + strings[i+1];
+    }
+    return result;
+    function escape(s) {
+        return s.replace(/&/g, "&amp;")
+                .replace(/</g, "&lt;")
+                .replace(/>/g, "&gt;")
+                .replace(/'/g, "&#039;")
+                .replace(/"/g, "&quot;")
+                .replace(/`/g, "&#096;");
+    }
+}
+var userinput = "<script>alert('test');</script>";
+var message = htmlEscape`<p>${userinput}</p>`;
+console.log(message);
+// → <p>&lt;script&gt;alert(&#039;test&#039;);&lt;/script&gt;</p>
