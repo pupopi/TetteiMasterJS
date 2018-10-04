69c6a6733947a14357201211a58e3dca63ae11db
pupopi <pupopipepan@gmail.com>
2018-10-04 11:52:26 +0900

    toString、toLocaleStringメソッド

---
+++ Chapter10/p277_002.js
@@ -0,0 +1,8 @@
+// toString、toLocaleStringメソッド
+
+// 要素を文字列に変換し、カンマで区切った文字列に
+// Localeでロケール固有の文字列に
+
+["A", "B", "C"].toString();     // → "A, B, C"
+[1, 2, 3].toString();           // → "1, 2, 3"
+[1, [2, 3]].toString();         // → "1, 2, 3"
