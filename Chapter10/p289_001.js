4dbd8ac16fd8dfcd914dd559432041258bb9a12c
pupopi <pupopipepan@gmail.com>
2018-10-04 15:42:39 +0900

    ECMAScript6:プロパティ名の省略

---
+++ Chapter10/p289_001.js
@@ -0,0 +1,5 @@
+// ECMAScript6:プロパティ名の省略
+
+{a, b} = {a: 1, b: 2};      // → {a: a, b: b} = {a: 1, b: 2}と同じ
+var {sin, cos, tan, PI} = Math;
+// var sin = Math.sin, cos = Math.cos, tan = Math.tan, PI = Math.PIと同じ
