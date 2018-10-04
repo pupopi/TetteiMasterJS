8278152a6c3171af9a4008a604af9ff60be196b7
pupopi <pupopipepan@gmail.com>
2018-10-04 16:20:10 +0900

    ECMAScript6:Mapデータを追加する

---
+++ Chapter10/p298_001.js
@@ -0,0 +1,6 @@
+// ECMAScript6:Mapデータを追加する
+
+var zip = new Map();
+zip.set("Tom", "131-8634");
+zip.set("Huck", "556-0002");
+console.log(zip);       // → Map {"Tom" => "131-8634", "Huck" => "556-0002"}
