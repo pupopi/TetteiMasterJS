c5b0e3bc049a9f7b6f69e03bb1433a468031be88
pupopi <pupopipepan@gmail.com>
2018-10-09 11:15:07 +0900

    大域脱出

---
+++ Chapter11/p320_001.js
@@ -0,0 +1,14 @@
+// 大域脱出
+
+var a = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+try {
+    a.forEach(function(v, i, a) {
+        if( i > 5 ) {
+            throw false;
+        }
+        return a[i] = v*v;
+    });
+} catch(e) {
+    if(e) throw e;
+}
+console.log(a);         // → [0, 1, 4, 9, 16, 25, 6, 7, 8, 9]
