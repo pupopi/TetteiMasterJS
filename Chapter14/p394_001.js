20831c131c4699ce6a727f7d74822dbeaff54691
pupopi <pupopipepan@gmail.com>
2018-10-15 11:14:22 +0900

    スクロール量を取得する関数

---
+++ Chapter14/p394_001.js
@@ -0,0 +1,16 @@
+// スクロール量を取得する関数
+
+function getScrollTop() {
+    if( window.pageYOffset !== undefined ) {
+        return window.pageYOffset;
+    } else {
+        return document.documentElement.scrollTop || document.body.scrollTop;
+    }
+}
+function getScrollLeft() {
+    if( window.pageXOffset !== undefined ) {
+        return window.pageXOffset;
+    } else {
+        return document.documentElement.scrollLeft || document.body.scrollLeft;
+    }
+}
