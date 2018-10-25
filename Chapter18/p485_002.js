880ee1ed6f16cf22a7f425f1523ece52c7eeb44a
pupopi <pupopipepan@gmail.com>
2018-10-25 14:36:00 +0900

    アクセッサプロパティnameを持つオブジェクトを生成するコンストラクタ

---
+++ Chapter18/p485_002.js
@@ -0,0 +1,17 @@
+// アクセッサプロパティnameを持つオブジェクトを生成するコンストラクタ
+
+function Person(name) {
+    Object.defineProperty(this, "name", {
+        get: function() {
+            return name;
+        },
+        set: function(newName) {
+            name = newName;
+        },
+        enumerable: true,
+        configurable: true
+    });
+}
+Person.prototype.sayName = function() {
+    console.log(this.name);
+};
